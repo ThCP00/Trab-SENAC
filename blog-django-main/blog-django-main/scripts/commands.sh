504a0880edc7df20d177259a11a95b40a958b724
Thiago Peres <180028278@aluno.unb.br>
2024-08-23 11:12:21 -0300

    Add files via upload

---
+++ blog-django-main/blog-django-main/scripts/commands.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# O shell irá encerrar a execução do script quando um comando falhar
+set -e
+
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+    echo "### Aguardando pelo inicio do Postgres Database($POSTGRES_HOST $POSTGRES_PORT) ###"
+    sleep 2
+done
+
+echo "@@@ Database iniciado com sucesso($POSTGRES_HOST:$POSTGRES_PORT) @@@"
+
+collectstatic.sh
+makemigrations.sh
+migrate.sh
+runserver.sh
